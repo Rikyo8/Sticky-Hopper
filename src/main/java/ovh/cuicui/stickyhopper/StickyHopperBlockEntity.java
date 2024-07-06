2d874d690c4c7b405b1284f53ac50e78e2ff9ead
Rikyo8 <167252054+Rikyo8@users.noreply.github.com>
2024-07-06 18:38:27 +0200

    StickyHopperBlockEntity.java

--- src/main/java/ovh/cuicui/stickyhopper/StickyHopperBlockEntity.java
+++ src/main/java/ovh/cuicui/stickyhopper/StickyHopperBlockEntity.java
@@ -1,29 +1,37 @@
 package ovh.cuicui.stickyhopper;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.HopperBlockEntity;
-import net.minecraft.text.Text;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class StickyHopperBlockEntity extends HopperBlockEntity {
-    public StickyHopperBlockEntity(BlockPos pos, BlockState state) { super(pos, state); }
+public class StickyHopperBlockEntity extends HopperBlockEntity implements Tickable {
+    public StickyHopperBlockEntity(BlockPos pos, BlockState state) {
+        super(Main.STICKY_HOPPER_BLOCK_ENTITY, pos, state);
+    }
 
     @Override
-    protected Text getContainerName() { return Text.translatable(Main.STICKY_HOPPER_CONTAINER_ID); }
+    public void tick() {
+        if (this.world != null && !this.world.isClient) {
+            // Custom filtering logic for the sticky hopper
+            for (int i = 0; i < this.size(); i++) {
+                ItemStack stack = this.getStack(i);
+                if (stack.isEmpty()) continue;
 
-    @Override
-    public BlockEntityType<?> getType() { return Main.STICKY_HOPPER_BLOCK_ENTITY; }
-
-    // A Sticky Hopper is considered empty even if there is still one item in some slots
-    @Override
-    public boolean isEmpty() {
-        this.checkLootInteraction(null); // Needs to be done first
-        for (int i = 0; i < this.size(); ++i) {
-            if (this.getStack(i).getCount() > 1) {
-                return (false);
+                // Example: Filter out all dirt blocks
+                if (stack.getItem() == Main.DIRT_ITEM) {
+                    this.setStack(i, ItemStack.EMPTY);
+                }
             }
+
+            // Ensure we call the original tick logic
+            HopperBlockEntity.serverTick(this.world, this.pos, this);
         }
-        return (true);
     }
 }
